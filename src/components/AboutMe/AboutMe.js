23ee46211b5532eb613d9f566cc06464c545c8fe
x-borz <KabanovRE@mosreg.ru>
2022-09-27 12:27:28 +0300

    feat. landing адаптив 768

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -8,15 +8,13 @@ function AboutMe(props) {
       <div className='about-me__container'>
         <SectionTitle modifier={'about-me__section-title'}>Студент</SectionTitle>
         <div className='about-me__student-dossier'>
-          <div className='about-me__student-info'>
-            <h3 className='about-me__student-name'>Ренат</h3>
-            <p className='about-me__student-profession'>Фронтенд-разработчик, 40 лет</p>
-            <p className='about-me__student-description'>Я родился и живу в городе Рыбинске Ярославской области.
-              Окончил Рыбинскую государственную авиационную технологическую академию, курс "Вычислительные системы" в 2003 году.
-              Женат, двое детей. Работаю Java-разработчиком. Завершаю курс "Веб-разработчик" от Яндекс.Практикум.
-            </p>
-            <a className='about-me__link' target='_blank' rel='noreferrer' href='https://github.com/x-borz'>Github</a>
-          </div>
+          <h3 className='about-me__student-name'>Ренат</h3>
+          <p className='about-me__student-profession'>Фронтенд-разработчик, 40 лет</p>
+          <p className='about-me__student-description'>Я родился и живу в городе Рыбинске Ярославской области.
+            Окончил Рыбинскую государственную авиационную технологическую академию, курс "Вычислительные системы" в 2003 году.
+            Женат, двое детей. Работаю Java-разработчиком. Завершаю курс "Веб-разработчик" от Яндекс.Практикум.
+          </p>
+          <a className='about-me__link' target='_blank' rel='noreferrer' href='https://github.com/x-borz'>Github</a>
           <img className='about-me__student-photo' alt='Ренат Кабанов' src={photo}/>
         </div>
       </div>
