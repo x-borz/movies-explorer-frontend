2b16aa3a3a7acdcc37b53f7ed0ce3ee7a7f6648f
x-borz <KabanovRE@mosreg.ru>
2022-09-29 15:41:00 +0300

    feat. Добавить компонент Register, Login. Res 768, 320

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -16,8 +16,8 @@ function App() {
       {/*<SavedMovies/>*/}
       {/*<Footer/>*/}
       {/*<NotFound/>*/}
-      <Register/>
-      {/*<Login/>*/}
+      {/*<Register/>*/}
+      <Login/>
     </div>
   );
 }
