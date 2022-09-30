896d723a34af6f57702a2a35c2ea9cc61801d95c
x-borz <KabanovRE@mosreg.ru>
2022-09-30 18:19:00 +0300

    feat. Добавил плавность открытия/закрытия бокового меню, закрытие меню по нажатию Escape

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,10 +1,22 @@
 import './Menu.css';
 import Navigation from "../Navigation/Navigation";
+import {useEffect} from "react";
 
 function Menu({isMenuVisible, onMenuClose}) {
+
+  useEffect(() => {
+    const handleEscClose = evt => {
+      if (evt.key === 'Escape') onMenuClose();
+    }
+    if (isMenuVisible) {
+      document.addEventListener("keydown", handleEscClose);
+      return () => document.removeEventListener("keydown", handleEscClose);
+    }
+  }, [isMenuVisible]);
+
   return (
     <section className={`menu ${isMenuVisible ? 'menu_visible' : ''}`}>
-      <div className='menu__panel'>
+      <div className={`menu__sidebar ${isMenuVisible ? 'menu__sidebar_visible' : ''}`}>
         <Navigation modifier='menu' onMenuClose={onMenuClose}/>
         <button className='menu__close-btn' type='button' onClick={onMenuClose}/>
       </div>
