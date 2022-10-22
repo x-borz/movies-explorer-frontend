bd99f99245d8dd8600c7632a6270ff74c616d684
x-borz <KabanovRE@mosreg.ru>
2022-10-22 11:01:03 +0300

    fix Menu scroll

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,21 +1,23 @@
 import './Menu.css';
 import Navigation from "../Navigation/Navigation";
-import {useEffect} from "react";
+import {useEffect, useRef} from "react";
 
 function Menu({isMenuVisible, onMenuClose}) {
+  const menuRef = useRef();
 
   useEffect(() => {
     const handleEscClose = evt => {
       if (evt.key === 'Escape') onMenuClose();
     }
     if (isMenuVisible) {
+      menuRef.current.scrollTo(0, 0);
       document.addEventListener("keydown", handleEscClose);
       return () => document.removeEventListener("keydown", handleEscClose);
     }
   }, [isMenuVisible]);
 
   return (
-    <section className={`menu ${isMenuVisible ? 'menu_visible' : ''}`}>
+    <section className={`menu ${isMenuVisible ? 'menu_visible' : ''}`} ref={menuRef}>
       <div className={`menu__sidebar ${isMenuVisible ? 'menu__sidebar_visible' : ''}`}>
         <Navigation modifier='menu' onMenuClose={onMenuClose}/>
         <button className='menu__close-btn' type='button' onClick={onMenuClose}/>
